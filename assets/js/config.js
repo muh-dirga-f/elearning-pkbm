3e0c83351c29dcf0e9b6b5f63e5956ce754dde88
Muh. Dirga F <muh.dirga.f@gmail.com>
2022-03-29 14:39:46 +0800

    fix asset

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,3 +1,4 @@
 // var url = "https://rest-api-elearning.000webhostapp.com/";
 //harus di set
-var url = "http://localhost/myapp/elearning";
+// var url = "http://localhost/myapp/elearning";
+var url = "https://elearning-pkbm-2022.herokuapp.com/";
